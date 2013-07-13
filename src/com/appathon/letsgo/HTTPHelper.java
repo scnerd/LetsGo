a25fc892d8e7ff9f12424bbb27fd2536d14c5d30
scnerd <scnerd@gmail.com>
2013-07-13 01:57:36 -0400

    Basic code for reading HTTP into JSON parser, and added required permission.

--- src/com/appathon/letsgo/HTTPHelper.java
+++ src/com/appathon/letsgo/HTTPHelper.java
@@ -1,43 +1,110 @@
 package com.appathon.letsgo;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
 public class HTTPHelper {
-	
-	//returns the User object corresponding to the sid 
-	public User GetUser(int sid)
-	{
+
+	private final String URL_GET_USER = "", URL_DEL_USER = "",
+			URL_CRT_USER = "", URL_GET_EVNT = "", URL_DEL_EVNT = "",
+			URL_CRT_EVNT = "", URL_ATN_EVNT = "";
+
+	static InputStream is = null;
+	static JSONObject jObj = null;
+	static String json = "";
+
+	public JSONObject getJSONFromUrl(String url) {
+
+		// Making HTTP request
+		try {
+			// defaultHttpClient
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(url);
+
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			is = httpEntity.getContent();
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			json = sb.toString();
+		} catch (Exception e) {
+			Log.e("Buffer Error", "Error converting result " + e.toString());
+		}
+
+		// try parse the string to a JSON object
+		try {
+			jObj = new JSONObject(json);
+		} catch (JSONException e) {
+			Log.e("JSON Parser", "Error parsing data " + e.toString());
+		}
+
+		// return JSON String
+		return jObj;
+
+	}
+
+	// returns the User object corresponding to the sid
+	public User GetUser(int sid) {
 		return null;
 	}
-	
-	//deletes the User object corresponding to the sid
-	//returns true if User was deleted successfully, false otherwise
-	public boolean DeleteUser(int sid)
-	{
+
+	// deletes the User object corresponding to the sid
+	// returns true if User was deleted successfully, false otherwise
+	public boolean DeleteUser(int sid) {
 		return false;
 	}
-	
-	
-	public boolean CreateUser(User user)
-	{
+
+	public boolean CreateUser(User user) {
 		return false;
 	}
-	
-	public Event GetEvent(int eventID)
-	{
+
+	public Event GetEvent(int eventID) {
 		return null;
 	}
-	
-	public boolean DeleteEvent(int eventID)
-	{
+
+	public boolean DeleteEvent(int eventID) {
 		return false;
 	}
-	
-	public boolean CreateEvent(Event event)
-	{
+
+	public boolean CreateEvent(Event event) {
 		return false;
 	}
-	
-	public boolean AttendEvent(Event event, User user)
-	{
+
+	public boolean AttendEvent(Event event, User user) {
 		return false;
 	}
 }
