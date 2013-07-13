d5951e5387e50da7b4a439db2c0ac4a293a569a1
mphox <michael.phox@gmail.com>
2013-07-13 09:56:18 -0400

    got 2 item list to work

--- src/com/appathon/letsgo/LetsGo_Main.java
+++ src/com/appathon/letsgo/LetsGo_Main.java
@@ -1,10 +1,15 @@
 package com.appathon.letsgo;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -21,8 +26,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TwoLineListItem;
 
 public class LetsGo_Main extends Activity {
 
@@ -41,8 +50,101 @@ public class LetsGo_Main extends Activity {
                 // TODO Auto-generated method stub
             }
         };
-        button.setOnClickListener(o);
+        button.setOnClickListener(o);final ListView listview = (ListView) findViewById(R.id.listEventItems);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this, /*R.layout.listview_event_list*/
+                android.R.layout.simple_list_item_2, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
     }
-    
+
+    private class EventNameArrayAdapter extends ArrayAdapter<Event> {
+        HashMap<Event, Integer> mIdMap = new HashMap<Event, Integer>();
+
+
+        public EventNameArrayAdapter(Context context, int textViewResourceId,
+                                  List<Event> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TwoLineListItem twoLineListItem;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) getContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                twoLineListItem = (TwoLineListItem) inflater.inflate(
+                        android.R.layout.simple_list_item_2, null);
+            } else {
+                twoLineListItem = (TwoLineListItem) convertView;
+            }
+
+            TextView text1 = twoLineListItem.getText1();
+            TextView text2 = twoLineListItem.getText2();
+
+            text1.setText("1");
+            text2.setText("2");
+            return twoLineListItem;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
 
 }
